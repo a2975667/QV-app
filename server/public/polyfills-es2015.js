4a9a7c9ced9a7da43994a57786a3e892f8eed7fc
Ti-Chung Cheng <tcheng10@illinois.edu>
2019-11-23 02:39:48 -0600

    change video layout

--- server/public/polyfills-es2015.js
+++ server/public/polyfills-es2015.js
@@ -6370,8 +6370,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\wl439\Documents\Code and Projects\QV\QV-app\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\wl439\Documents\Code and Projects\QV\QV-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /mnt/d/13_Bash/QV-app/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /mnt/d/13_Bash/QV-app/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
